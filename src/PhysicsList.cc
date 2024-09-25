76014b1a4faa6648546986b0db41627e89a36201
Charlie-BrownGT <108026345+Charlie-BrownGT@users.noreply.github.com>
2024-09-25 21:53:09 +0100

    Add files via upload

---
+++ src/PhysicsList.cc
@@ -0,0 +1,10 @@
+#include "PhysicsList.hh"
+
+MyPhysicsList::MyPhysicsList()
+{
+	RegisterPhysics (new G4EmStandardPhysics());
+	RegisterPhysics (new G4OpticalPhysics());
+}
+
+MyPhysicsList::~MyPhysicsList()
+{}
